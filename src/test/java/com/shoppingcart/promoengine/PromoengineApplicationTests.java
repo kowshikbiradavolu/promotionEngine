fac87fe4153e453277960ad2217e3ef1460f4c30
US\kkowshikreddy <kkowshikreddy@deloitte.com>
2021-10-04 14:04:26 +0530

    Initial Package Changes -- Generated using spring initializr

---
+++ src/test/java/com/shoppingcart/promoengine/PromoengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.promoengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
